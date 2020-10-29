111f67fc133f1087980764fbf300a5df254c5629
Frances9 <44355133+Frances9@users.noreply.github.com>
2020-10-29 15:21:27 +0800

    Update basicRead.ino

--- example/basicRead/basicRead.ino
+++ example/basicRead/basicRead.ino
@@ -1,7 +1,7 @@
 /*!
  * @file basicRead.ino
- * @brief 输出红光和红外光的读数
- * @n 本示例支持的主板有ESP8266、FireBeetle-M0、UNO、ESP32、Leonardo 、Mega2560
+ * @brief Output readings of red light and IR 
+ * @n This library supports mainboards: ESP8266, FireBeetle-M0, UNO, ESP32, Leonardo, Mega2560
  * @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  * @licence     The MIT License (MIT)
  * @author [YeHangYu](hangyu.ye@dfrobot.com)
@@ -15,7 +15,7 @@
 DFRobot_MAX30102 particleSensor;
 
 /*
-传感器配置中使用的宏定义选项
+Macro definition opions in sensor configuration 
 sampleAverage: SAMPLEAVG_1 SAMPLEAVG_2 SAMPLEAVG_4 
                SAMPLEAVG_8 SAMPLEAVG_16 SAMPLEAVG_32
 ledMode:       MODE_REDONLY  MODE_RED_IR  MODE_MULTILED
@@ -26,10 +26,10 @@ adcRange:      ADCRANGE_2048 ADCRANGE_4096 ADCRANGE_8192 ADCRANGE_16384
 */
 void setup()
 {
-  //串口初始化
+  //Init serial 
   Serial.begin(115200);
   /*!
-   *@brief 传感器初始化
+   *@brief Init sensor 
    *@param pWire IIC bus pointer object and construction device, can both pass or not pass parameters (Wire in default)
    *@param i2cAddr Chip IIC address (0x57 in default)
    *@return true or false
@@ -40,13 +40,13 @@ void setup()
   }
 
   /*!
-   *@brief 传感器配置，使用给出的宏定义进行配置
-   *@param ledBrightness LED灯的亮度，默认值0x1F（6.4mA），取值范围: 0~255（0=Off ，255=50mA）
-   *@param sampleAverage 多个样本平均后抽取一次，减少数据吞吐量，默认4个样本平均
-   *@param ledMode LED灯的模式，默认同时使用红光和红外光
-   *@param sampleRate 采样速率，默认每秒取400个样本
-   *@param pulseWidth 脉冲宽度，脉冲宽度越长，探测范围就越大，默认最大范围
-   *@param adcRange ADC量程，默认4096 (nA)，15.63(pA) per LSB
+   *@brief Use macro definition to configure sensor
+   *@param ledBrightness LED brightness, default value: 0x1F（6.4mA), Range: 0~255（0=Off, 255=50mA）
+   *@param sampleAverage Average multiple samples then draw once, reduce data throughput, default 4 samples average
+   *@param ledMode LED mode, default to use red light and IR at the same time 
+   *@param sampleRate Sampling rate, default 400 samples every second 
+   *@param pulseWidth Pulse width: the longer the pulse width, the wider the detection range. Default to be Max range
+   *@param adcRange Measurement Range, default 4096 (nA), 15.63(pA) per LSB
    */
   particleSensor.sensorConfiguration(/*ledBrightness=*/0x1F, /*sampleAverage=*/SAMPLEAVG_4, \
                                   /*ledMode=*/MODE_MULTILED, /*sampleRate=*/SAMPLERATE_400, \
@@ -55,17 +55,17 @@ void setup()
 
 void loop()
 {
-  //打印结果
+  //Print result
   Serial.print("R=");
   /*!
-   *@brief 获得red值
-   *@return 红光读数
+   *@brief Get red value
+   *@return Red light reading 
    */
   Serial.print(particleSensor.getRed());
   Serial.print(" IR=");
   /*!
-   *@brief 获得IR值
-   *@return 红外光读数
+   *@brief Get IR value 
+   *@return IR reading 
    */
   Serial.print(particleSensor.getIR());
   Serial.println();
