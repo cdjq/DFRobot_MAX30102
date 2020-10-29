cf2aee7e9777e2c26c946ca53acc2cce7956a761
Frances9 <44355133+Frances9@users.noreply.github.com>
2020-10-29 15:15:38 +0800

    Update SPO2.ino

--- example/SPO2/SPO2.ino
+++ example/SPO2/SPO2.ino
@@ -1,8 +1,8 @@
 /*!
  * @file SPO2.ino
- * @brief 在串口上显示实时心率和SPO2水平，正常人的血氧浓度范围在95~100之间
- * @n 最好用橡皮筋把传感器固定在手指上，手指按压力度的变化会影响结果
- * @n 本示例支持的主板有ESP8266、FireBeetle-M0、UNO、ESP32、Leonardo 、Mega2560
+ * @brief Display heart-rate and SPO2 on serial in real-time, normal SPO2 is within 95~100
+ * @n Try to fix the sensor on your finger in using to avoid the effect of pressure change on data output.
+ * @n This library supports mainboards: ESP8266, FireBeetle-M0, UNO, ESP32, Leonardo, Mega2560
  * @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  * @licence     The MIT License (MIT)
  * @author [YeHangYu](hangyu.ye@dfrobot.com)
@@ -16,7 +16,7 @@
 DFRobot_MAX30102 particleSensor;
 
 /*
-传感器配置中使用的宏定义选项
+Macro definition options in sensor configuration
 sampleAverage: SAMPLEAVG_1 SAMPLEAVG_2 SAMPLEAVG_4
                SAMPLEAVG_8 SAMPLEAVG_16 SAMPLEAVG_32
 ledMode:       MODE_REDONLY  MODE_RED_IR  MODE_MULTILED
@@ -27,10 +27,10 @@ adcRange:      ADCRANGE_2048 ADCRANGE_4096 ADCRANGE_8192 ADCRANGE_16384
 */
 void setup()
 {
-  //串口初始化
+  //Init serial
   Serial.begin(115200);
   /*!
-   *@brief 传感器初始化
+   *@brief Init sensor 
    *@param pWire IIC bus pointer object and construction device, can both pass or not pass parameters (Wire in default)
    *@param i2cAddr Chip IIC address (0x57 in default)
    *@return true or false
@@ -41,29 +41,29 @@ void setup()
   }
 
   /*!
-   *@brief 传感器配置，使用给出的宏定义进行配置
-   *@param ledBrightness LED灯的亮度，默认值0x1F（6.4mA），取值范围: 0~255（0=Off ，255=50mA）
-   *@param sampleAverage 多个样本平均后抽取一次，减少数据吞吐量，默认4个样本平均
-   *@param ledMode LED灯的模式，默认同时使用红光和红外光
-   *@param sampleRate 采样速率，默认每秒取400个样本
-   *@param pulseWidth 脉冲宽度，脉冲宽度越长，探测范围就越大，默认最大范围
-   *@param adcRange ADC量程，默认4096 (nA)，15.63(pA) per LSB
+   *@brief Use macro definition to configure sensor 
+   *@param ledBrightness LED brightness, default value: 0x1F（6.4mA), Range: 0~255（0=Off, 255=50mA）
+   *@param sampleAverage Average multiple samples then draw once, reduce data throughput, default 4 samples average
+   *@param ledMode LED mode, default to use red light and IR at the same time 
+   *@param sampleRate Sampling rate, default 400 samples every second 
+   *@param pulseWidth Pulse width: the longer the pulse width, the wider the detection range. Default to be Max range
+   *@param adcRange ADC Measurement Range, default 4096 (nA), 15.63(pA) per LSB
    */
   particleSensor.sensorConfiguration(/*ledBrightness=*/50, /*sampleAverage=*/SAMPLEAVG_4, \
                         /*ledMode=*/MODE_MULTILED, /*sampleRate=*/SAMPLERATE_100, \
                         /*pulseWidth=*/PULSEWIDTH_411, /*adcRange=*/ADCRANGE_16384);
 }
 
-int32_t SPO2; //血氧浓度
-int8_t SPO2Valid; //显示SPO2计算是否有效的标志
-int32_t heartRate; //心率
-int8_t heartRateValid; //显示心率计算是否有效的标志
+int32_t SPO2; //SPO2
+int8_t SPO2Valid; //Flag to display if SPO2 calculation is valid
+int32_t heartRate; //Heart-rate
+int8_t heartRateValid; //Flag to display if heart-rate calculation is valid 
 
 void loop()
 {
   Serial.println(F("Wait about four seconds"));
   particleSensor.heartrateAndOxygenSaturation(/**SPO2=*/&SPO2, /**SPO2Valid=*/&SPO2Valid, /**heartRate=*/&heartRate, /**heartRateValid=*/&heartRateValid);
-  //打印结果
+  //Print result 
   Serial.print(F("heartRate="));
   Serial.print(heartRate, DEC);
   Serial.print(F(", heartRateValid="));
